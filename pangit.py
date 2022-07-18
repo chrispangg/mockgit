090c5beca331feee030e64cc25bc5a14632f9e2d
Chris Pang <chris.pang@chorus.co.nz>
2022-07-19 10:20:19 +1200

    added three methods for finding and or creating path to file or directory

--- pangit.py
+++ pangit.py
@@ -48,3 +48,63 @@ def main(argv=sys.argv[1:]):
     elif args.command == "tag":
         cmd_tag(args)
 
+
+class GitRepository(object):
+    # A git repository
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=None):
+        # Use init to check if git repo is valid. Use 'force' to bypass checks
+        # valid repo will have .git
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git Repo %s" % path)
+
+        # Read config file in .git/config using configparser
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")  # repo_file gets the path to config
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
+
+
+def repo_path(repo, *path):
+    # Compute path under repo's gitdir
+    return os.path.join(repo.gitdir, *path)
+
+
+def repo_file(repo, *path, mkdir=False):
+    # Same as repo_path but creates dirname(*path) to file using repo_dir if absent.
+    # For example, repo_file(r, \"refs\", \"remotes\", \"origin\", \"HEAD\") will create .git/refs remotes/origin.
+
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    # Make directory to the path if absent and if mkdir
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None

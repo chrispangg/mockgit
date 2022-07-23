db4fe4234317582467330854ee5d8c67d228c294
Chris Pang <chris.pang@chorus.co.nz>
2022-07-23 15:32:59 +1200

    added GitIndexEntry index

--- pangit.py
+++ pangit.py
@@ -911,3 +911,34 @@ def cmd_ref_parse(args):
     repo = repo_find()
 
     print(object_find(repo, args.name, fmt, follow=True))
+
+class GitIndexEntry(object):
+    ctime = None
+    """The last time a file's metadata changed.  This is a tuple (seconds, nanoseconds)"""
+
+    mtime = None
+    """The last time a file's data changed.  This is a tuple (seconds, nanoseconds)"""
+
+    dev = None
+    """The ID of device containing this file"""
+    ino = None
+    """The file's inode number"""
+    mode_type = None
+    """The object type, either b1000 (regular), b1010 (symlink), b1110 (gitlink). """
+    mode_perms = None
+    """The object permissions, an integer."""
+    uid = None
+    """User ID of owner"""
+    gid = None
+    """Group ID of ownner (according to stat 2.  Isn'th)"""
+    size = None
+    """Size of this object, in bytes"""
+    obj = None
+    """The object's hash as a hex string"""
+    flag_assume_valid = None
+    flag_extended = None
+    flag_stage = None
+    flag_name_length = None
+    """Length of the name if < 0xFFF (yes, three Fs), -1 otherwise"""
+
+    name = None
